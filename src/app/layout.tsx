7fb8518d7a4f9adfde49fe2bfabff7933186eb2e
Muhamad Salman <salmanfatih448@gmail.com>
2025-09-03 16:22:19 +0700

    change tittle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import Header from "@/components/header";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "My Awesome Portfolio",
+  title: "My Portfolio",
   description: "Portfolio created with Next.js",
 };
 
